e35eeb591e18366428d53739ce9d383d698f090b
Sant90 <44289662+Sant90@users.noreply.github.com>
2018-11-14 09:51:15 +0530

    Add files via upload

---
+++ conformitycheck/src/main/java/com/scb/conformitycheck/utils/ReceiverConstants.java
@@ -0,0 +1,8 @@
+package com.scb.conformitycheck.utils;
+
+public interface ReceiverConstants {
+	public static final String CONFORMITY_CHECK_URL = "/conformitycheck";
+	public static final String CONFORMITY_CHECK_REQUEST_HANDLE_URL = "/conformitycheckRequestHandler";
+	public static final String[] TRANSMITTER_REQUEST_HANDLE_URL = null;
+
+}
